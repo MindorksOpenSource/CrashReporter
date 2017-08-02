0996481e7d4ae3eeb5741e35dd7b78fce623c820
balsikandar <bal.sikandar@yahoo.com>
2017-08-03 02:41:34 +0530

    Add flex layout

--- app/src/main/java/com/balsikandar/crashreporter/sample/MainActivity.java
+++ app/src/main/java/com/balsikandar/crashreporter/sample/MainActivity.java
@@ -1,12 +1,9 @@
 package com.balsikandar.crashreporter.sample;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.balsikandar.crashreporter.CrashReporter;
 
